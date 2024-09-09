3dff494494cc88f05536998faf577f3ce3967d6e
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-09-09 20:15:22 +0530

    Update home.py

--- home.py
+++ home.py
@@ -4,19 +4,17 @@ from PIL import Image
 def home_page():
     st.title("📈 Welcome to the Netflix Stock Price Prediction App")
 
-    # Welcome message with styling
+    # Welcome message with updated styling
     st.markdown("""
     <div style="text-align: center;">
-        <h2>📊 Predict Netflix Stock Prices with Ease!</h2>
-        <p style="font-size: 1.2em; color: #555;">This app allows you to forecast Netflix stock prices based on historical data. Navigate through the app to explore its features and insights.</p>
+        <p style="font-size: 1.2em; color: #555;">Forecast Netflix stock prices using historical data. Explore the app through the sidebar to find various features and insights.</p>
     </div>
     """, unsafe_allow_html=True)
     
-
     # Add a call-to-action or additional information
     st.markdown("""
     <div style="text-align: center; margin-top: 20px;">
         <h3>Get Started:</h3>
-        <p style="font-size: 1.1em;">Use the sidebar to navigate to different sections of the app. Start with the <strong>Predict</strong> page to make predictions on Netflix stock prices based on historical data.</p>
+        <p style="font-size: 1.1em;">Navigate to the <strong>Predict</strong> page to begin forecasting Netflix stock prices.</p>
     </div>
     """, unsafe_allow_html=True)
