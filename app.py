6037820e534a116fd62b7dcd4fad9b949b2796fc
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-09-09 20:12:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from insights import insights_page
 from feedback import feedback_page
 from about import about_page
 from PIL import Image
+    # Load and display an image with enhanced styling
+image = Image.open("gold.jpg")  # Replace with your image path
+st.image(image, caption="📈 Data-Driven Insights for Netflix Stocks", use_column_width=True)
 
 # Custom CSS to style the app
 st.markdown("""
