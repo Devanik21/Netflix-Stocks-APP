515b2478120588fc3c46aa3df4ed5f1573405201
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-09-11 19:50:55 +0530

    Update about.py

--- about.py
+++ about.py
@@ -5,7 +5,7 @@ def about_page():
     st.markdown("""
         <style>
         .about-container {
-            background-color: #f0f8ff;  /* Light AliceBlue background */
+            background-color: #402e22;  /* Light AliceBlue background */
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
